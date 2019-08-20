12b8edf69234259405cf593bc1f45092c060b3d6
pxyco <andvalderrama@gmail.com>
2019-08-20 19:58:35 +0000

    Funcionando con marcar leidos a las malas

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :text, length: { maximum: 140 }
+  # validates :text, length: { maximum: 140 }
+  validates :text, presence: true, length: { maximum: 140 }
 end
