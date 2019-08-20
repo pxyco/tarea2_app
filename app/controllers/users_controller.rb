31b953eb52213bc6ac7fa25b9b448bc26fb16bc0
pxyco <andvalderrama@gmail.com>
2019-08-20 05:53:27 +0000

    Con mark as read

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    @posts = Post.all
+    if current_user
+      pag = '/users/'
+      pag.concat(current_user.id.to_s)
+      redirect_to pag
+    end
+  end
+  
+  def show
+    @posts = Post.all
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @posts = Post.all
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    # numPosts = "1, 2, 3"
+    numPosts = current_user.prueba + ", " +  params[:user][:prueba]
+    # numPosts = params[:user][:prueba]
+    pag = '/users/'
+    pag.concat(current_user.id.to_s)
+    # current_user.update_attributes(params[:user])
+    current_user.update_attribute(:prueba, numPosts)
+    redirect_to pag
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  match '/users/:id',     to: 'users#show',       via: 'get'
+  # resources :users, :only =>[:show, :edit]
+  
+  resources :users
   resources :posts
-  root to: "pages#home"
+  root to: "users#index"
 
-  devise_for :users
+  # devise_for :users
+  devise_for :users, :path_prefix => 'd'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
